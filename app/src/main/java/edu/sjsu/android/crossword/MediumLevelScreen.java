81f6db3017daee14513482e422b4bc37ebc242dd
pfarrow75 <pfarrow75@gmail.com>
2023-05-09 20:34:31 -0700

    Added medium level, crashes when selecting choose difficulty after win.

--- app/src/main/java/edu/sjsu/android/crossword/MediumLevelScreen.java
+++ app/src/main/java/edu/sjsu/android/crossword/MediumLevelScreen.java
@@ -1,12 +1,23 @@
 package edu.sjsu.android.crossword;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
+import android.os.CountDownTimer;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +26,14 @@ import android.view.ViewGroup;
  */
 public class MediumLevelScreen extends Fragment {
 
+    private StringBuilder selectedLetters = new StringBuilder();
+    Set<String> submittedAnswers = new HashSet<>();
+    int correctAnswerSubmitted = 0;
+    int score = 0;
+
+    private CountDownTimer timer;
+
+    String[] correctWords = {"BATCH", "BRACT", "CHART", "BAT", "CHAT", "CART", "ACT"};
     public MediumLevelScreen() {
         // Required empty public constructor
     }
@@ -37,6 +56,182 @@ public class MediumLevelScreen extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_medium_level_screen, container, false);
+        View view = inflater.inflate(R.layout.fragment_medium_level_screen, container, false);
+
+
+        final MediaPlayer mp = MediaPlayer.create(getContext(), R.raw.bingo);
+        final MediaPlayer complete = MediaPlayer.create(getContext(), R.raw.complete);
+        final MediaPlayer wrong = MediaPlayer.create(getContext(), R.raw.wrong_answer);
+        final MediaPlayer contain = MediaPlayer.create(getContext(), R.raw.error_sound);
+        TextView letterR = view.findViewById(R.id.m_R);
+        TextView letterC = view.findViewById(R.id.m_C);
+        TextView letterH = view.findViewById(R.id.m_H);
+        TextView letterA = view.findViewById(R.id.m_A);
+        TextView letterT = view.findViewById(R.id.m_T);
+        TextView letterB = view.findViewById(R.id.m_B);
+        TextView guessInput = view.findViewById(R.id.guessInput_m);
+        Button submitButton = view.findViewById(R.id.submitBtn_m);
+        TextView e_score = view.findViewById(R.id.m_score);
+        TextView timerTextView = view.findViewById(R.id.m_timer);
+
+        timer = new CountDownTimer(120000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                // Update the timer display with the remaining time
+                timerTextView.setText("Time: " + millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+                TimeOutFragment dialogFragment = new TimeOutFragment();
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+                dialogFragment.show(fragmentManager, "TimeOutFragment");
+            }
+        };
+        timer.start();
+
+        View.OnClickListener letterClickListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String letter = ((TextView) v).getText().toString();
+                populateGuessInput(guessInput, letter);
+            }
+        };
+
+        letterR.setOnClickListener(letterClickListener);
+        letterC.setOnClickListener(letterClickListener);
+        letterA.setOnClickListener(letterClickListener);
+        letterB.setOnClickListener(letterClickListener);
+        letterT.setOnClickListener(letterClickListener);
+        letterH.setOnClickListener(letterClickListener);
+
+        TextView one1 = view.findViewById(R.id.m_1_1);
+        TextView one2 = view.findViewById(R.id.m_1_2);
+        TextView one3 = view.findViewById(R.id.m_1_3);
+        TextView two1 = view.findViewById(R.id.m_2_1);
+        TextView two2 = view.findViewById(R.id.m_2_2);
+        TextView two4 = view.findViewById(R.id.m_2_4);
+        TextView three1 = view.findViewById(R.id.m_3_1);
+        TextView three2 = view.findViewById(R.id.m_3_2);
+        TextView three3 = view.findViewById(R.id.m_3_3);
+        TextView three4 = view.findViewById(R.id.m_3_4);
+        TextView three5 = view.findViewById(R.id.m_3_5);
+        TextView three6 = view.findViewById(R.id.m_3_6);
+        TextView four1 = view.findViewById(R.id.m_4_1);
+        TextView four2 = view.findViewById(R.id.m_4_2);
+        TextView four3 = view.findViewById(R.id.m_4_3);
+        TextView four4 = view.findViewById(R.id.m_4_4);
+        TextView five1 = view.findViewById(R.id.m_5_1);
+        TextView five2 = view.findViewById(R.id.m_5_2);
+        TextView five4 = view.findViewById(R.id.m_5_4);
+        TextView five5 = view.findViewById(R.id.m_5_5);
+        TextView six1 = view.findViewById(R.id.m_6_1);
+        TextView six2 = view.findViewById(R.id.m_6_2);
+        TextView six3 = view.findViewById(R.id.m_6_3);
+
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String guess = guessInput.getText().toString();
+                if (!guess.isEmpty() && !submittedAnswers.contains(guess)){
+                    int correctIndex = -1;
+                    for (int i = 0; i < correctWords.length; i++){
+                        if (guess.equals(correctWords[i])){
+                            correctIndex = i;
+                            correctAnswerSubmitted +=1;
+                            submittedAnswers.add(guess);
+                            break;
+                        }
+                    }
+
+                    if (correctIndex != -1){
+                        mp.start();
+                        int remainingTime = Integer.parseInt(timerTextView.getText().toString().substring(6));
+                        score += remainingTime * 10;
+                        e_score.setText("Score : " + score);
+                        switch (correctWords[correctIndex]){
+                            case "BAT":
+                                updateTextView(one1, "B");
+                                updateTextView(one2, "A");
+                                updateTextView(one3, "T");
+                                break;
+                            case "CHAT":
+                                updateTextView(two1, "C");
+                                updateTextView(two2, "H");
+                                updateTextView(four2, "A");
+                                updateTextView(two4, "T");
+                                break;
+                            case "BRACT":
+                                updateTextView(three1, "B");
+                                updateTextView(three2, "R");
+                                updateTextView(three3, "A");
+                                updateTextView(three4, "C");
+                                updateTextView(one3, "T");
+                                break;
+                            case "BATCH":
+                                updateTextView(three1, "B");
+                                updateTextView(three5, "A");
+                                updateTextView(four4, "T");
+                                updateTextView(five1, "C");
+                                updateTextView(three6, "B");
+                                break;
+                            case "CART":
+                                updateTextView(four1, "C");
+                                updateTextView(four2, "A");
+                                updateTextView(four3, "R");
+                                updateTextView(four4, "T");
+                                break;
+                            case "CHART":
+                                updateTextView(five1, "C");
+                                updateTextView(five2, "H");
+                                updateTextView(six1, "A");
+                                updateTextView(five4, "R");
+                                updateTextView(five5, "T");
+                                break;
+                            case "ACT":
+                                updateTextView(six1, "A");
+                                updateTextView(six2, "C");
+                                updateTextView(six3, "T");
+                                break;
+                        }
+                        if (correctAnswerSubmitted == correctWords.length){
+                            complete.start();
+                            timer.cancel();
+                            ScoreFragment scoreFragment = new ScoreFragment();
+                            // Set any data that you want to pass to the fragment using arguments
+                            Bundle args = new Bundle();
+                            args.putInt("score", score);
+                            scoreFragment.setArguments(args);
+                            // Show the fragment using the FragmentManager
+                            FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                            scoreFragment.show(fragmentManager, "score");
+                        }
+                    } else {
+                        wrong.start();
+                    }
+                } else {
+                    contain.start();
+                }
+                guessInput.setText("");
+                selectedLetters = new StringBuilder("");
+            }
+        });
+
+        return view;
+    }
+    private void populateGuessInput(TextView guessInput, String letter) {
+        // Append the selected letter to the StringBuilder
+        selectedLetters.append(letter);
+
+        // Set the text of the guessInput TextView to the current selection
+        guessInput.setText(selectedLetters.toString());
+    }
+
+    private void updateTextView(TextView textView, String text) {
+        textView.setText(text);
+        textView.setTextSize(15); // set text size to 20sp
+        textView.setGravity(Gravity.CENTER); // center text horizontally and vertical
+        textView.setTypeface(Typeface.DEFAULT_BOLD);
+        textView.setTextColor(Color.BLACK);
     }
 }
